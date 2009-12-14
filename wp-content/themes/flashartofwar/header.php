72cfbe0393265bbb74ad0def4ac63de11e157e10
Jesse Freeman <jesse@flashbum.com>
2009-12-14 09:45:50 -0600

    Removed hardcoded feedback widget js

--- wp-content/themes/flashartofwar/header.php
+++ wp-content/themes/flashartofwar/header.php
@@ -21,19 +21,6 @@
   document.write(unescape("%3Cscript src='" + asset_host + "javascripts/feedback-v2.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
 
-<script type="text/javascript" charset="utf-8">
-  var feedback_widget_options = {};
-
-  feedback_widget_options.display = "overlay";  
-  feedback_widget_options.company = "flash_art_of_war";
-  feedback_widget_options.placement = "right";
-  feedback_widget_options.color = "#222";
-  feedback_widget_options.style = "idea";
-
-  var feedback_widget = new GSFN.feedback_widget(feedback_widget_options);
-</script>
-
-
 </head>
 
 <body>
